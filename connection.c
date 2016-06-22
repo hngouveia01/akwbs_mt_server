dc99718ee40d29986d728ff45e6b5e2616d01615
Henrique Nascimento Gouveia <h.gouveia@icloud.com>
2016-06-22 11:51:41 -0300

    connection: functions to send and receive data
    These functions send to and receive from
    clients. They adjust pointers related to
    the ring buffers. The speed rate of the
    sending data is managed here.

--- connection.c
+++ connection.c
@@ -107,3 +107,75 @@ static int get_timeout(struct akwbs_connection *connection)
 
   return AKWBS_SUCCESS;
 }
+
+/*!
+ * Receive more data from a socket and put into the given buffer.
+ *
+ * \param buffer buffer to store data into.
+ * \param socket_descriptor socket descriptor to get data from.
+ *
+ * \return AKWBS_SUCCESS on success receiving data from the given socket.
+ *         AKWBS_ERROR on error while trying to get data from the given socket.
+ */
+static int recv_data_from_socket(struct akwbs_connection *connection)
+{
+  ssize_t bytes_read     = 0;
+  size_t  free_space     = 0;
+  void    *write_address = NULL;
+
+
+  free_space = ring_buffer_count_free_bytes(&connection->buffer);
+
+  if (free_space == 0)
+    return RING_BUFFER_IS_FULL;  /* This should never occur while receiving headers. */
+
+  write_address = ring_buffer_write_address(&connection->buffer);
+
+  bytes_read    = recv(connection->client_socket, write_address, free_space, 0);
+
+  if (bytes_read == AKWBS_ERROR)
+    return AKWBS_ERROR;
+
+  gettimeofday(&connection->last_activity, NULL);
+
+  ring_buffer_write_advance(&connection->buffer, bytes_read);
+
+  return AKWBS_SUCCESS;
+}
+
+
+/*!
+ * Send data to a socket and update the buffer accounting.
+ *
+ */
+static int send_data_to_socket(struct akwbs_connection *connection)
+{
+  ssize_t bytes_sent      = 0;
+  size_t  bytes_to_send   = 0;
+  void    *read_address   = NULL;
+  int     ret             = 0;
+
+
+  bytes_to_send = ring_buffer_count_bytes(&connection->buffer);
+
+  if (bytes_to_send == 0)
+    return AKWBS_SUCCESS;
+
+  ret = manage_send_rate(connection, &bytes_to_send);
+
+  if (ret == -2)
+    return AKWBS_SUCCESS;
+
+  read_address = ring_buffer_read_address(&connection->buffer);
+
+  bytes_sent = send(connection->client_socket, read_address, bytes_to_send, 0);
+
+  if (bytes_sent == AKWBS_ERROR)
+    return AKWBS_ERROR;
+
+  connection->bytes_sent_last_io += bytes_sent;
+
+  ring_buffer_read_advance(&connection->buffer, bytes_sent);
+
+  return AKWBS_SUCCESS;
+}
